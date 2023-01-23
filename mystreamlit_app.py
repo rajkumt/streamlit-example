602b2dc28180baa0263378d0b65a3e422db83bf9
rajkumt <39495159+rajkumt@users.noreply.github.com>
2023-01-23 03:05:22 -0800

    update mystreamapp
    added 3 items in menu

--- mystreamlit_app.py
+++ mystreamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("my parents healthy dinner")
 st.header('Breakfast Menu')
-st.text('Kale spinach, rocket smothie')
-st.text('hard boiled Egg free range egg')
-
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
