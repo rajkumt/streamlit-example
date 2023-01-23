52a3e5a01c054d35faba9eeae736b7cdc4aec3b0
rajkumt <39495159+rajkumt@users.noreply.github.com>
2023-01-23 03:21:32 -0800

    update mystreamlit_app
    updated my stream lit app

--- mystreamlit_app.py
+++ mystreamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas
+import pandas as pd
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 st.title("my parents healthy dinner")
@@ -8,5 +8,5 @@ st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
