6cfac054fee6e1a98b2b94efc3c8161a1a4360aa
rajkumt <39495159+rajkumt@users.noreply.github.com>
2023-01-23 03:15:23 -0800

    update mystreamlit aoo
    updated image content

--- mystreamlit_app.py
+++ mystreamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 st.title("my parents healthy dinner")
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
