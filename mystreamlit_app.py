d0bc14ef66ba5a5cf50b8ab2eb2b0d657a7f114b
rajkumt <39495159+rajkumt@users.noreply.github.com>
2023-02-08 09:50:32 -0800

    stremlit ref corrected
    code fix

--- mystreamlit_app.py
+++ mystreamlit_app.py
@@ -22,7 +22,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruit_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 
 streamlit.dataframe(fruit_to_show)
