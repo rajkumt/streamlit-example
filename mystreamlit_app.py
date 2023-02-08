18d49de5f01e4002a155088944ed470bafc915a6
rajkumt <39495159+rajkumt@users.noreply.github.com>
2023-02-08 09:30:32 -0800

    changed alias name
    updaetd streamlit

--- mystreamlit_app.py
+++ mystreamlit_app.py
@@ -1,20 +1,20 @@
-import streamlit as st
+import streamlit
 import pandas as pd
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.title("my parents healthy dinner")
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.title("my parents healthy dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -25,6 +25,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 
-st.dataframe(fruit_to_show)
+streamlit.dataframe(fruit_to_show)
+
 
 
